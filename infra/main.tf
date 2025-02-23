3f72ad73992b7b1f54257061c350accba3936a86
Cameron <camnowil96@gmail.com>
2025-02-23 18:17:58 -0500

    Updated main.tf to include config code for cloudfront, s3, dynamodb, and route53. Code complete.

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,110 @@ resource "aws_lambda_function" "cloudresumetest-api" {
   handler          = "view_counter.lambda_handler"
   runtime          = "python3.13"
 }
+resource "aws_dynamodb_table" "cloudresume-test" {
+  name           = "cloudresume-test"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"  
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_s3_bucket" "cloud-resume-challenge-cnw" {
+  bucket = "cloud-resume-challenge-cnw"
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_policy" {
+  bucket = aws_s3_bucket.cloud-resume-challenge-cnw.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.cloud-resume-challenge-cnw.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "cloud-resume-challenge-cnw.s3.us-east-1.amazonaws.com"
+  description                       = "OAC for CloudFront to access S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.cloud-resume-challenge-cnw.bucket_regional_domain_name}"
+    origin_id   = "S3-cloud-resume-challenge-cnw"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["cameronnwilson.com", "www.cameronnwilson.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-cloud-resume-challenge-cnw"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+price_class = "PriceClass_100"
+
+restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+}
+
+viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:051826710466:certificate/e298a867-8cb5-4ab8-ab37-6a4480ead708"
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+  }
+}
+
+resource "aws_route53_zone" "main" {
+  name = "cameronnwilson.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "cameronnwilson.com"
+  type    = "A"
+  alias {
+    name                   = "dyzxmqdljo8xc.cloudfront.net."
+    zone_id                = "Z2FDTNDATAQYW2"  
+    evaluate_target_health = false
+  }
+}
 
 resource "aws_iam_role" "CloudResume" {
   name        = "CloudResume"
@@ -32,7 +136,8 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
   name        = "aws_iam_policy_for_terraform_resume_project_policy"
   path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
-  policy      = jsonencode({
+  policy      = jsonencode(
+  {
     Version = "2012-10-17"
     Statement = [
       {
